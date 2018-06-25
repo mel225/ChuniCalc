e3238b3377b55a0762b412f2414759156c29ce18
casge <39999551+mel225@users.noreply.github.com>
2018-06-26 03:39:34 +0900

    Update getMaxChain.js

--- docs/getMaxChain.js
+++ docs/getMaxChain.js
@@ -108,7 +108,7 @@
         var mainSection = getList_FoundByArray(elements, function(e){return(e.className=='t-line-img');})[0];
         console.log("check end function getList_FoundByArray()");
         elements = getArrayByList(mainSection.children);
-        var docTables = getList_FoundByArray(elements, function(e){return(e.namedItem['table'];});
+        var docTables = getList_FoundByArray(elements, function(e){return(e.namedItem['table']});
         tables.push(getList_FoundByArray(docTables, function(t){
           console.log(t);
           return (t.rows[0].length > 1);
