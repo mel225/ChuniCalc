b8cf4afad7f1c40db81fdae256d5776cf09ea7fd
mel225 <mini20cat_drumer@outlook.jp>
2018-06-27 17:31:56 +0900

    innerTextでよかったらしい

--- docs/getMaxChain.js
+++ docs/getMaxChain.js
@@ -149,7 +149,7 @@
           for(j=0; j<colLen; j++){
             html = table.rows[i].cells[j];
             if(onceExe){
-              console.log("data-col: " + html.getAttribute('data-col'), "innerHTML: " + html.innerHTML);
+              console.log("data-col: " + html.getAttribute('data-col'), "innerText: " + html.innerText);
             }
             // music = new MusicData(title);
           }
