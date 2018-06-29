c6a7a390a0e05765a332858f1e28fbb4cbb94981
mel225 <mini20cat_drumer@outlook.jp>
2018-06-29 15:03:59 +0900

    ファイルの読み込みを試しにXMLHttpRequestで書いてみた。動けぇぇぇ

---
+++ docs/readFile.js
@@ -0,0 +1,24 @@
+(function(){
+    alert(openFile());
+    
+    function openFile(){
+	var reader;
+	return new promise(function(resolve, reject){
+	    var xhr = new XMLHttpRequest();
+	    xhr.responseType = "text";
+	    xhr.addEventListener("load", function(){
+		resolve(xhr.response);
+	    });
+	    xhr.addEventListener("error", function(){
+		console.log("Error occared");
+		reject();
+	    });
+	    xhr.open('GET', "https://mel225.github.io/ChuniCalc/ChuniNotesDataTable.txt", true);
+	    xhr.send("");
+	}).catch((e)=>{
+	    console.log(e);
+	});
+    };
+    
+	
+}(document);
