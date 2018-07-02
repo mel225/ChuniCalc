a29b6eba15a0c7fae9efcb583a3ca74d7b3272b1
mel225 <mini20cat_drumer@outlook.jp>
2018-07-02 14:53:11 +0900

    ブックマークレットを作ってなかったので作成。

---
+++ docs/readFileBR.js
@@ -0,0 +1,5 @@
+javascript:(function(d, s){
+  s = d.createElement('script');
+  s.src = "https://mel225.github.io/ChuniCalc/readFile.js";
+  d.getElementsByTagName('head')[0].appendChild(s);
+}) (document)
